95ef2a3554d6d08f00c63de489050e5e4f3c3792
Qudus Lawal <lawalqudus980@gmail.com>
2022-09-09 12:43:24 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Start Execution
+ *
+ * description - "Programming is like building a multilingual partner
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
