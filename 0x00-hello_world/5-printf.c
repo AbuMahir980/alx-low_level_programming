c6091f0e815ef1db88e6650e745df79c003a021e
Qudus Lawal <lawalqudus980@gmail.com>
2022-09-09 20:39:24 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Point of Entry
+ *
+ * description - with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: (0) for success Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
