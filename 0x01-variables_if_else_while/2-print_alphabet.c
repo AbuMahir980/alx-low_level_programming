e625c8daac834daec4032e9790ccd4c5a3a7d8b9
Qudus Lawal <lawalqudus980@gmail.com>
2022-09-12 10:38:41 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * description - prints the alphabet in lower case/n
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
