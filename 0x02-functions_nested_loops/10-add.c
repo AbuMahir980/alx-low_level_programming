8f6af42b19318a46d1ad85f1547df1a212814e8b
Qudus Lawal <lawalqudus980@gmail.com>
2022-09-13 11:56:55 -0700

    two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * description: adds two numbers
+ *
+ * Return: Always 0 success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
